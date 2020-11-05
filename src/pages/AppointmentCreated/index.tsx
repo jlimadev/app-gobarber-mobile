2dfbe885cf41b0c026d210535d84d5222b9b2da7
jlimadev <jlima.dev@gmail.com>
2020-11-05 11:05:09 -0300

    sucess page created

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,8 +1,52 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useCallback, useMemo } from 'react';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale/pt-BR';
+import {
+  Container,
+  Title,
+  Description,
+  OkButton,
+  OkButtonText,
+} from './styles';
 
-const Profile: React.FC = () => {
-  return <View />;
+interface RouteParams {
+  date: number;
+}
+
+const AppointmentCreated: React.FC = () => {
+  const { reset } = useNavigation();
+  const { params } = useRoute();
+
+  const routeParams = params as RouteParams;
+
+  const okPressedHandler = useCallback(() => {
+    reset({
+      routes: [{ name: 'Dashboard' }],
+      index: 0,
+    });
+  }, [reset]);
+
+  const formattedDate = useMemo(() => {
+    return format(
+      routeParams.date,
+      "EEEE', dia' dd 'de' MMMM 'de' yyyy 'às' HH:mm'h",
+      { locale: ptBR },
+    );
+  }, [routeParams.date]);
+  return (
+    <Container>
+      <Icon name="check" size={80} color="#04d361" />
+
+      <Title>Agendado com Sucesso!</Title>
+      <Description>{formattedDate}</Description>
+
+      <OkButton onPress={okPressedHandler}>
+        <OkButtonText>OK</OkButtonText>
+      </OkButton>
+    </Container>
+  );
 };
 
-export default Profile;
+export default AppointmentCreated;
